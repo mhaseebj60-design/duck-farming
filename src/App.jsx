4a58dbcf49de7027f31474c96c62638eaa5c6513
mhaseebj60-design <mhaseebj60@gmail.com>
2026-08-19 15:04:14 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div style={{ padding: '40px', color: 'black', background: 'white' }}>
-      <h1>Duck Farming Test</h1>
-      <p>If you can see this, React is working.</p>
+    <div>
+      <h1>Duck Farming</h1>
+      <p>Welcome to our Duck Farming website.</p>
     </div>
   );
 }
